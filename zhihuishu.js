fe38ee505db16b8174a3a6a6464e823303e82042
3000MALL <65356537+3000MALL@users.noreply.github.com>
2020-05-18 12:01:43 +0800

    Update zhihuishu.js

--- zhihuishu.js
+++ zhihuishu.js
@@ -21,7 +21,7 @@ function drawWindow(){
 	$('head').append('<link href="https://ghcdn.rawgit.org/LDS-Skeleton/OnlineCourseScript/master/main.css?t='+new Date().getTime()+'" rel="stylesheet" type="text/css" />');
 	//标签拼接
 	$("body").append("<div id='skdiv'></div>");
-	$("#skdiv").html("<p ><span style='font-weight:bold;    font-size: large;'>智慧树刷课脚本</span>（可用鼠标拖动）<p><p>版本：20200517 广西外国语学院市营专1906班</p><div id='content' style='   border-top: 2px solid;'></div>");
+	$("#skdiv").html("<p ><span style='font-weight:bold;    font-size: large;'>智慧树刷课脚本</span>（可用鼠标拖动）<p><p>版本：20200518 广西外国语学院市营专1906班</p><div id='content' style='   border-top: 2px solid;'></div>");
 	$('#content').html('<div ><p  id="rate_txt" >当前播放速度：默认1.25倍速</p><button id="startplay" onclick="start()">刷课任务加载成功，正在刷课..</button>');
 	$('#content').html($('#content').html()+"<div style='margin-top:10px'><p style='font-weight:bold'>当前进度:&nbsp;&nbsp;<span id='progress'>0%</span></p><hr></hr><p  id='cp'>当前章节：</p><p id='np'>下一章节：</p></div>");
 
