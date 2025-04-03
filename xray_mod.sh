4e0b0a8eefacf8ebef72f2665166cbed41660849
3000MALL <65356537+3000MALL@users.noreply.github.com>
2025-04-03 17:53:42 +0800

    Update xray_mod.sh

--- xray_mod.sh
+++ xray_mod.sh
@@ -37,7 +37,7 @@ V6_PROXY=""
 IP=`curl -sL -4 ip.sb`
 if [[ "$?" != "0" ]]; then
     IP=`curl -sL -6 ip.sb`
-    V6_PROXY="https://gh.3000mall.com/"
+    V6_PROXY="https://gh.hijk.art/"
 fi
 
 BT="false"
