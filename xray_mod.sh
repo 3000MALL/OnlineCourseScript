6cce14d553812bfac45d06b853f902008d73d27d
3000MALL <65356537+3000MALL@users.noreply.github.com>
2025-04-16 11:43:17 +0800

    Update xray_mod.sh

--- xray_mod.sh
+++ xray_mod.sh
@@ -1791,12 +1791,12 @@ showLog() {
 menu() {
     clear
     echo "#############################################################"
-    echo -e "#                   ${RED}Xray一键安装脚本${PLAIN}                #"
-    echo -e "# ${GREEN}作者${PLAIN}: 3000mall(CPLA_54J)                    #"
-    echo -e "# ${GREEN}网址${PLAIN}: https://3000mall.com                  #"
-    echo -e "# ${GREEN}论坛${PLAIN}: https://bbs.3000mall.com              #"
-    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/3000mallclub            #"
-    echo -e "# ${RED}注意${PLAIN}: 本脚本仅内部，请不要外传，谢谢！                        #"
+    echo -e "#                   ${RED}Xray一键安装脚本${PLAIN}			#"
+    echo -e "# ${GREEN}作者${PLAIN}: 3000mall(CPLA_54J)				#"
+    echo -e "# ${GREEN}网址${PLAIN}: https://3000mall.com			#"
+    echo -e "# ${GREEN}论坛${PLAIN}: https://bbs.3000mall.com			#"
+    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/3000mallclub			#"
+    echo -e "# ${RED}注意${PLAIN}: 本脚本仅内部，请不要外传，谢谢！		#"
     echo -e "#                                                           #"	
     echo "#############################################################"
     echo -e "  ${GREEN}1.${PLAIN}   安装Xray-${BLUE}VMESS+WS+TLS${PLAIN}${RED}(推荐)${PLAIN}"
