5ef238ed0df2e84b38b6dcea3a5688b4db547d28
3000MALL <65356537+3000MALL@users.noreply.github.com>
2025-06-11 18:41:00 +0800

    Update xray_mod.sh

--- xray_mod.sh
+++ xray_mod.sh
@@ -14,20 +14,15 @@ colorEcho() {
 }
 
 # 以下网站是随机从Google上找到的无广告小说网站，不喜欢请改成其他网址，以http或https开头
-# 搭建好后无法打开伪装域名，可能是反代小说网站挂了，请在网站留言，或者Github发issue，以便替换新的网站
 SITES=(
 http://www.zhuizishu.com/
 http://xs.56dyc.com/
-#http://www.xiaoshuosk.com/
-#https://www.quledu.net/
 http://www.ddxsku.com/
 http://www.biqu6.com/
 https://www.wenshulou.cc/
-#http://www.auutea.com/
 http://www.55shuba.com/
 http://www.39shubao.com/
 https://www.23xsw.cc/
-#https://www.huanbige.com/
 https://www.jueshitangmen.info/
 https://www.zhetian.org/
 http://www.bequgexs.com/
@@ -486,7 +481,8 @@ getData() {
                 PROXY_URL="https://bing.imeizi.me"
                 ;;
             5)
-                while true; do
+                while true
+                do
                     read -p " 请输入反代站点(以http或者https开头)：" PROXY_URL
                     if [[ -z "$PROXY_URL" ]]; then
                         colorEcho $RED " 请输入反代网站！"
